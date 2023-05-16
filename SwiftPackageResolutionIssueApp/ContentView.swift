c12c831c26e14d624edd29ec3ce206d06302d8bd
Todd Thomas <toddwthomas@me.com>
2023-05-16 09:26:26 -0600

    Initial Commit

---
+++ SwiftPackageResolutionIssueApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftPackageResolutionIssueApp
+//
+//  Created by Todd Thomas on 5/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftPackageResolutionIssueApp/SwiftPackageResolutionIssueAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPackageResolutionIssueAppApp.swift
+//  SwiftPackageResolutionIssueApp
+//
+//  Created by Todd Thomas on 5/16/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPackageResolutionIssueAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
